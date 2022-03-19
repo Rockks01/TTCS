8d07aa49e701e061786a461bcb001650f0e024d4
Mr-Dino <kropachevsemen15@gmail.com>
2022-03-19 14:35:59 +0300

    "user"

--- TTCS/core/models.py
+++ TTCS/core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class OurUser(AbstractUser):
+    first_name = models.CharField(max_length=255, verbose_name="Имя")
+    surname = models.CharField(max_length=255, verbose_name="Фамилия", db_index=True)
+    username = models.CharField(max_length=50, unique=True, db_index=True, verbose_name="Никнейм")
+    email = models.EmailField(verbose_name='Электронная почта', unique=True)
+    # phone = models.CharField(max_length=11, verbose_name="Номер телефона", unique=True) Стоит доработать посмотреть если поле шаблона для телефона
+    is_customer = models.BooleanField(default=False, verbose_name="Заказчик")
+    is_employer = models.BooleanField(default=False, verbose_name="Исполнитель")
+    data_joined = models.DateTimeField(auto_now_add=True, verbose_name="Дата регистрации")
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        order_by = ('surname', )
