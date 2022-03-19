0ce0dadd6c26f874c5db068e49c8a66e88ec281e
amtp1 <magomedov.abdul20012@gmail.com>
2022-03-19 13:46:46 +0300

    TTCS

--- TTCS/core/urls.py
+++ TTCS/core/urls.py
@@ -2,7 +2,9 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path("", Web.index, name="index")
+    path("", Web.index, name="index"),
+    path("signin", Web.signin, name="signin"),
+    path("signup", Web.signup, name="signup")
 ]
 
 handler505 = "core.views"
--- TTCS/core/views.py
+++ TTCS/core/views.py
@@ -4,7 +4,16 @@ from django.shortcuts import render
 
 class Web:
     def index (request):
-        return render(request, "index.html")
+        if not request.user.is_authenticated:
+            return render(request, "index.html")
+        else:
+            return render(request, "profile.html")
+
+    def signin(request):
+        return render(request, "signin.html")
+
+    def signup(request):
+        return render(request, "signup.html")
 
 def handler505(request):
     type_, value, traceback = sys.exc_info()
